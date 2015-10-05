6f893ac92fabf62d638636bf31fdf059b0340b0c
Skyvory <skyvory@hotmail.com>
2015-10-05 16:47:07 +0800

    now

---
+++ database/seeds/CommentTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->delete();
+    
+        Comment::create(array(
+            'author' => 'Chris Sevilleja',
+            'text' => 'Look I am a test comment.'
+        ));
+    
+        Comment::create(array(
+            'author' => 'Nick Cerminara',
+            'text' => 'This is going to be super crazy.'
+        ));
+    
+        Comment::create(array(
+            'author' => 'Holly Lloyd',
+            'text' => 'I am a master of Laravel and Angular.'
+        ));
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $this->call(CommentTableSeeder::class);
+        $this->command->info('Comment table seeded.');
 
         Model::reguard();
     }
