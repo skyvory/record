83136fb736303516efe7c0c4c7763c7afbea444f
Skyvory <skyvory@hotmail.com>
2015-10-09 15:04:45 +0800

    add relation

--- app/User.php
+++ app/User.php
@@ -11,29 +11,42 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements AuthenticatableContract,
-                                    AuthorizableContract,
-                                    CanResetPasswordContract
+									AuthorizableContract,
+									CanResetPasswordContract
 {
-    use Authenticatable, Authorizable, CanResetPassword;
+	use Authenticatable, Authorizable, CanResetPassword;
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name', 'email', 'password'];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = ['password', 'remember_token'];
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = ['password', 'remember_token'];
+
+	// user has many assessments
+	protected function assessments() {
+		return $this->hasMany('App\Assessments', 'user_id');
+	}
+	// user has many notes
+	protected function notes() {
+		return $this->hasMany('App\Notes', 'user_id');
+	}
+	// user has many stocks
+	protected function stocks() {
+		return $this->hasMany('App\Stocks', 'user_id');
+	}
 }
