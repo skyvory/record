bbb79a6f46467106f5a34db5b8f241e8e4e8261c
Skyvory <skyvory@hotmail.com>
2015-10-12 14:23:04 +0800

    update records

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+
 use App\User;
+use App\Vn;
+use App\Developer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -42,6 +45,95 @@ class DatabaseSeeder extends Seeder
 		{
 			User::create($user);
 		}
+
+		DB::table('developers')->delete();
+		$devs = array(
+			[
+				'id' => '1',
+				'name_en' => 'grmblgrmbl',
+				'name_jp' => 'mzmz'
+			],
+		);
+		foreach($devs as $dev) {
+			Developer::create($dev);
+		}
+	
+		 DB::table('vn')->delete();
+		 $vn = array(
+		 	[
+		 		'title_en' => 'version test alpha',
+		 		'title_jp' => 'alpha test verson',
+		 		'hashtag' => 'alpha',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test beta',
+		 		'title_jp' => 'beta test verson',
+		 		'hashtag' => 'beta',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test charlie',
+		 		'title_jp' => 'charlie test verson',
+		 		'hashtag' => 'charlie',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test delta',
+		 		'title_jp' => 'delta test verson',
+		 		'hashtag' => 'delta',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test echo',
+		 		'title_jp' => 'echo test verson',
+		 		'hashtag' => 'echo',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test foxtrot',
+		 		'title_jp' => 'foxtrot test verson',
+		 		'hashtag' => 'foxtrot',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test golf',
+		 		'title_jp' => 'golf test verson',
+		 		'hashtag' => 'golf',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test hotel',
+		 		'title_jp' => 'hotel test verson',
+		 		'hashtag' => 'hotel',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test indo',
+		 		'title_jp' => 'indo test verson',
+		 		'hashtag' => 'indo',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 	[
+		 		'title_en' => 'version test juliet',
+		 		'title_jp' => 'juliet test verson',
+		 		'hashtag' => 'juliet',
+		 		'developer_id' => '1',
+		 		'date_release' => '2015-06-06 13:23:03'
+		 	],
+		 );
+		 foreach($vn as $v) {
+		 	Vn::create($v);
+		 }
 
 		Model::reguard();
 	}
