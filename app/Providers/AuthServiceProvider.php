edc7c1d88fedbbd9e8604253782bce3d453c51b6
Skyvory <skyvory@hotmail.com>
2015-10-13 17:26:59 +0800

    define abilities for assessment

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,25 +7,51 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		'App\Model' => 'App\Policies\ModelPolicy',
+	];
 
-    /**
-     * Register any application authentication / authorization services.
-     *
-     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
-     * @return void
-     */
-    public function boot(GateContract $gate)
-    {
-        parent::registerPolicies($gate);
+	/**
+	 * Register any application authentication / authorization services.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+	 * @return void
+	 */
+	public function boot(GateContract $gate)
+	{
+		parent::registerPolicies($gate);
 
-        //
-    }
+		//
+		// $gate->before(function($user, $ability) {
+		// 	if($user->isAdmin()) {
+		// 		return true;
+		// 	}
+		// });
+		$gate->define('add-vn', function($user, $vn) {
+			return $user->role == "common";
+		});
+		$gate->define('index-assessment', function($user, $assessment) {
+			return $user->role == "common";
+		});
+		$gate->define('show-assessment', function($user, $assessment) {
+			return $user->role == "common";
+		});
+		$gate->define('store-assessment', function($user, $assessment) {
+			return $user->role == "common";
+		});
+		$gate->define('update-assessment', function($user, $assessment) {
+			if($user->role == "common" && $user->id == $assessment->user_id) {
+				return true;
+			}
+			return false;
+		});
+		$gate->define('delete-assessment', function($user, $assessment) {
+			return $user->role == "common";
+		});
+	}
 }
