0a6b75c7f197e512dc650100797726c1433bae7b
Skyvory <skyvory@hotmail.com>
2016-01-08 01:35:10 +0800

    Increase VNDB character fetch result limit, fix issue #1

--- app/Http/Controllers/VndbController.php
+++ app/Http/Controllers/VndbController.php
@@ -41,7 +41,8 @@ class VndbController extends Controller
 		$client = new Client();
 		$client->connect();
 		$client->login($username = $request->input('username'), $password = $request->input('password'));
-		$res = $client->sendCommand('get character basic,details,meas,traits (vn = ' . (int)$request->input('vndb_id') . ')');
+		$options = json_encode(array('results' => 25));
+		$res = $client->sendCommand('get character basic,details,meas,traits (vn = ' . (int)$request->input('vndb_id') . ') ' . $options);
 		$res_after = json_decode(json_encode($res), true);
 		return response()->json($res_after);
 	}
