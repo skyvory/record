c626653142311d774b2369e10b3cf6395261448b
Skyvory <skyvory@hotmail.com>
2016-10-19 12:47:35 +0800

    Enable cross-site origin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,11 @@
 <?php
 
+// appended header to forcefully enable cors
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+// end of forecefull header append
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
