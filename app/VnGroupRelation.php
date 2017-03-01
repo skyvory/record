0d4fcfa52adcc34ee08607174e08d89a5eb8e647
Rika Voice <skyvory@hotmail.com>
2017-03-01 17:20:41 +0800

    Update Laravel to 5.2.x

--- app/VnGroupRelation.php
+++ app/VnGroupRelation.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class VnGroupRelation extends Model
 {
     protected $table = 'vn_group_relations';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
 }
