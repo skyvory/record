5d80de1c9ef5617770c15c44fb37006c7eb3ec00
Rika Voice <skyvory@hotmail.com>
2017-03-08 13:51:51 +0800

    Add constant configuration file

---
+++ config/constant.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'USE_PROXY' => false,
+    'PROXY_URL' => 'https://quantum.000webhostapp.com/',
+    // 'PROXY_URL' => 'https://skyvory.net/',
+    'USE_REQUEST_ENCRYPTION' => true,
+    'ENCRYPTION_HASH' => '16a6d7f49404004f737be38f9caec915a411a5380ea1604edbaf34ebc398f6a4'
+];
