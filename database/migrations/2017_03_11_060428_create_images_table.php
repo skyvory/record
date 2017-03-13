419c36e3106a5afb21ef7e5259ac7518452d3e89
Rika Voice <skyvory@hotmail.com>
2017-03-13 19:44:38 +0800

    Add images table migration

---
+++ database/migrations/2017_03_11_060428_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('images',function(Blueprint $table) {
+			$table->increments('id');
+			$table->integer('vn_id')->unsigned();
+			$table->string('local_name');
+			$table->string('image_url')->nullable();
+			$table->smallInteger('screen')->nullable()->comment('1:title, 2:gameplay, 3:config, 4:save/load, 5:omake');
+			$table->string('description', 600)->nullable();
+			$table->timestamp('created_at')->useCurrent();
+			$table->timestamp('updated_at')->useCurrent();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('images');
+	}
+}
