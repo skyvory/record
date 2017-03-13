26ee846dd949e8ee29be38f9c2918499dd4a0422
Rika Voice <skyvory@hotmail.com>
2017-03-13 23:53:46 +0800

    Rename Image model/migration to Screen
    There's already internal library that uses Image as namespace and thus
    creating conflict that breaks the app

--- app/Screen.php
+++ app/Screen.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Image extends Model
+class Screen extends Model
 {
-    protected $table = 'images';
+    protected $table = 'screens';
 }
--- database/migrations/2017_03_11_060428_create_screens_table.php
+++ database/migrations/2017_03_11_060428_create_screens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImagesTable extends Migration
+class CreateScreensTable extends Migration
 {
 	/**
 	 * Run the migrations.
@@ -13,7 +13,7 @@ class CreateImagesTable extends Migration
 	 */
 	public function up()
 	{
-		Schema::create('images',function(Blueprint $table) {
+		Schema::create('screens',function(Blueprint $table) {
 			$table->increments('id');
 			$table->integer('vn_id')->unsigned();
 			$table->string('original_filename');
@@ -33,6 +33,6 @@ class CreateImagesTable extends Migration
 	 */
 	public function down()
 	{
-		Schema::drop('images');
+		Schema::drop('screens');
 	}
 }
